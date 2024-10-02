010dbe934253f0dd1b247766b7b8a50913f412dd
calc6 <calum.cullen66@gmail.com>
2024-10-02 17:16:51 +0100

    add priority

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -7,16 +7,17 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false ,priority:"high"},
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority:"low" },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false , priority:"Medium"}
     ]
   });
 
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority:""
   });
 
   const doneHandler = (taskIndex) => {
@@ -43,6 +44,9 @@ function App() {
         break;
       case "deadline":
         form.deadline = event.target.value;
+        break;
+      case "priority":
+        form.priority = event.target.value;
         break;
       default:
         form = formState;
@@ -73,6 +77,7 @@ function App() {
           description={task.description}
           deadline={task.deadline}
           key={task.id}
+          priority={task.priority}
           done={task.done}
           markDone={() => doneHandler(index)}
           deleteTask = {() => deleteHandler(index)}
